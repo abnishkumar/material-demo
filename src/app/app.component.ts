beee85eade80ea35d865559c170cf3be9cdffa29
Abnish Kumar <abnish.kumar4@hotmail.com>
2018-05-14 16:42:40 +0530

    use formgroup in place of formcontrol

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { FormControl, Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators, FormGroup, FormBuilder } from '@angular/forms';
 import { dataSource } from './controls/data';
 import { TextBoxType } from './controls/controls.module';
 import { State } from './controls/state.model';
@@ -10,20 +10,35 @@ import { State } from './controls/state.model';
   styleUrls: ['./app.component.scss']
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit {
 
-
-  title = 'app';
-  autoCtrl: FormControl = new FormControl('Arkansas', Validators.required);
-  textboxCtrl: FormControl = new FormControl('', Validators.required);
-  emailFormCtrl: FormControl = new FormControl('', [Validators.required, Validators.email]);
-  telPhoneCtrl: FormControl = new FormControl('', [Validators.required]);
-  chkCtrl: FormControl = new FormControl();
-  dateCtrl: FormControl = new FormControl();
-  telType = TextBoxType.telephone;
-  data: State[] = dataSource;
+  group: FormGroup;
+  data: State[];
+  telType = '';
   template = `<img style="vertical-align:middle;" aria-hidden src="{flag}" height="25" />
 <span>{name}</span> |
 <small>Population: {population}</small>`;
+  title = 'app';
+  /**
+   *
+   */
+  constructor(private fb: FormBuilder) {
+    this.telType = TextBoxType.telephone;
+    this.data = dataSource;
 
+    this.group = this.fb.group({
+      autoCtrl: new FormControl('', Validators.required),
+      textboxCtrl: new FormControl('', Validators.required),
+      emailFormCtrl: new FormControl('', [Validators.required, Validators.email]),
+      telPhoneCtrl: new FormControl('', [Validators.required]),
+      chkCtrl: new FormControl(),
+      dateCtrl: new FormControl()
+    });
+  }
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.group.controls['autoCtrl'].setValue('Arkansas');
+    }, 1000);
+  }
 }
